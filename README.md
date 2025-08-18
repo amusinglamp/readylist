eb2a6494c1cb07d5c5c15f1b5064368bd700cfb2
Christopher Butt <chris@sennit.io>
2025-08-18 12:38:28 +0100

    added graphviz to prepare for output, instructions to the readme

--- README.md
+++ README.md
@@ -8,11 +8,17 @@ A state graph editor.
 pip install .
 ```
 
+Also need sudo apt-get install graphviz libgraphviz-dev 
+
 ## Usage
+source ./venv/bin/activate
 readylist-cli --greet Alice
 readylist-cli --version
 
 ## Development
+source ./venv/bin/activate
+
 Run tests with 
-pip install -e .[test]
+pip install -e . This builds a wheel using the pyproject.toml file)
 pytest
+
--- src/readylist/cli.py
+++ src/readylist/cli.py
@@ -1,11 +1,12 @@
 import argparse
+import pygraphviz
 from importlib.metadata import version
 from .core import ReadyList
 
 def main():
     parser = argparse.ArgumentParser(description="ReadyList CLI")
     parser.add_argument("--greet", type=str, help="Greet the specified name")
-    parser.add_argument("--version", action="version", version=f"readylist-cli {version('readylist-cli')}")
+    parser.add_argument("--version", action="version", version=f"readylist-cli {version('readylist')}")
     args = parser.parse_args()
 
     readylist = ReadyList()
